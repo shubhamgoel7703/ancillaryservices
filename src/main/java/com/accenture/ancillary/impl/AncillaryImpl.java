01a474c970097493e0aeecd4d7bb259f50334133
athreyamurthy <athreya.aths@gmail.com>
2016-08-18 21:01:56 +0530

    adding jar for mysql and getters

--- src/main/java/com/accenture/ancillary/impl/AncillaryImpl.java
+++ src/main/java/com/accenture/ancillary/impl/AncillaryImpl.java
@@ -8,6 +8,13 @@ import com.accenture.ancillary.service.AncillaryService;
 public class AncillaryImpl implements AncillaryService{
 	
 	AncillaryDataDAL dataDAL;
+	
+	public AncillaryDataDAL getDataDAL() {
+		return dataDAL;
+	}
+	public void setDataDAL(AncillaryDataDAL dataDAL) {
+		this.dataDAL = dataDAL;
+	}
 	private static final Logger log = Logger.getLogger(AncillaryDataDAL.class);
 	@Override
 	public String testMethod() {
